dcbf35da43d56b582dedaf85329cb9d054f18788
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2023-12-18 15:24:34 +0100

    add router

---
+++ routing/routes/messages.router.js
@@ -0,0 +1,3 @@
+const express = require('express');
+
+const messagesController = require('../controllers/messages.controller.js');
--- routing/server.js
+++ routing/server.js
@@ -1,43 +1,29 @@
 
 const express = require('express');
 
-// We import the controllers
-const friendsController = require('./controllers/friends.controller.js');
-const messagesController = require('./controllers/messages.controller.js');
 
+
+// NEW. Import the router
+const friendsRouter = require('./routes/friends.router.js');
+//const messagesRouter = require('./routes/messages.router.js');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-/*
-// The old way of doing things was to have this here.
-// But with MVC, we move this part to the models.
-
-const friends = [{
-        id: 0,
-        name: 'Alice'
-    },
-    {
-        id: 1,
-        name: 'Bob'
-    },
-    {
-        id: 2,
-        name: 'Charlie'
-    }
-];
-
-*/
 
 app.use(express.json());
 
-// notice how much cleaner this is than the previous version!!!
+/* 
+// Old way
 app.post('/friends', friendsController.postFriend);
-
 app.get('/friends', friendsController.getFriends);
-
 app.get('/friends/:friendId', friendsController.getFriend);
+// ------------------------------
+*/
+// New Way
+app.use('/friends', friendsRouter); // mount the router
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}...`);
