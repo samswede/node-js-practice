88d8dc1a856236265899bd6e6960d9e0f604e494
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2023-12-18 11:07:59 +0100

    update

--- basic_http_server/parameterised_urls.cjs
+++ basic_http_server/parameterised_urls.cjs
@@ -25,7 +25,15 @@ server.on('request', (req, res) => {
     const items = req.url.split('/');
     // e.g. /friends/1 => ['', 'friends', '1']
 
-    if (items[1] === 'friends') { // checks second item in array
+    if (req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (chunk) => {
+            const friend = JSON.parse(chunk.toString()); // converts buffer to string to json
+            console.log('Request: ', friend);
+            friends.push(friend);
+            res.end(JSON.stringify(friend));
+        });
+
+    } else if (req.method === 'GET' && items[1] === 'friends') { // checks second item in array
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         if (items.length === 3) { // if its 3, then it means there is an id
@@ -47,3 +55,31 @@ server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}...`);
 
 });
+
+// example of a POST request:
+// curl -X POST -d '{"id": 4, "name": "Eve"}' -H "Content-Type: application/json" http://localhost:3000/friends
+
+/* 
+
+// Paste the following into the console while at the /friends endpoint:
+// You should see the following in the terminal here:
+// Request:  { id: 4, name: 'Eve' }
+// and the following in the console:
+// { id: 4, name: 'Eve' }
+// and when you refresh the browser, you should see the following:
+// [{"id":1,"name":"Alice"},{"id":2,"name":"Bob"},{"id":3,"name":"Charlie"},{"id":4,"name":"Eve"}]
+
+// note that eve was added to the end of the array
+
+fetch('http://localhost:3000/friends', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        id: 4,
+        name: 'Eve'
+    })
+}).then(res => res.json()).then(data => console.log(data)); 
+
+*/
